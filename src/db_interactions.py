cbf0f95d878c4b1f6bc0b8a64eeae8a72b218bf7
Kopan <aakk123221@gmail.com>
2024-03-10 20:47:44 +0100

    Made the main return json responses
    Also added fetching the data from database as well as some resource methods.

--- src/db_interactions.py
+++ src/db_interactions.py
@@ -1,7 +1,36 @@
 import sqlite3
-import time
 
-def main(): pass
+"""
+Based on single_day variable fetches records from db
+"""
+#yyyy-mm-dd
+#Assembles the query based on the single_day flag
+def assemble_query(data:dict, single_day:bool,company:str) -> str:
+    if single_day: #SQLite query for a single date
+        query = f"""
+        SELECT *
+        FROM {company}
+        WHERE Date
+        = \"{data['date']}\";"""
+    else: #SQLite query for a range of dates
+        query = f"""
+        SELECT *
+        FROM {company}
+        WHERE Date
+        BETWEEN \'{data['date_s']}\'
+        AND \'{data['date_e']}\';"""
+    return query
 
-if __name__=='__main__':
-    main()
+#Fetches the records from db
+def get_records(data:dict,db_path:str, single_day:bool) -> list: 
+    #cursor and connection to the database
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    response = []
+    for company in data['companies']:
+        #Assembling and executing of the query
+        query = assemble_query(data,single_day,company)
+        rows = cur.execute(query).fetchall()
+        response.append((company,rows))
+    return response
+ 
---
+++ src/resources.py
@@ -0,0 +1,74 @@
+from .db_interactions import get_records
+
+
+wrong_arg_message:dict = dict(
+    cod=404,
+    message = "Wrong arguments provided. For more info..."
+)
+no_data_message:dict = dict(
+    cod=404,
+    message = "There is no existing record for the date provided"
+)
+
+company_names:list = ["Apple", "Alphabet", "Amazon", "Netflix", "Meta Platform"]
+help_message:dict = dict(
+    cod = 200,
+    data = dict(
+    Company = "Folder for list of companies, separated by &",
+    Day = "Singular day",
+    Days ="Range of days",
+    Companies_args = company_names,
+    Templates = [
+        "URL/companies/<companies>?date=12-12-2012>",
+        "URL/companies/<companies>?date_start=...&date_finish=..."
+    ]
+    )
+)
+
+#Get and verify if the url is correct, otherwise rise an exception
+#When an exception is raised, the main will return wrong args message
+def filter_data(data:dict,db_path:str) -> list:
+    #Check if all companies are intact.
+    #If any is wrong, return wrong message
+    for company in data['companies']:
+        if not company in company_names:
+            raise Exception("Wrong arguments.")
+
+    #If a singular date was provided, return it no matter
+    #if the range of dates was provided aswell
+    if data['date']:
+        rows = get_records(data,db_path=db_path,single_day=True)
+    elif data['date_s'] and data['date_e']:
+        rows = get_records(data,db_path=db_path,single_day=False)
+    else: raise Exception
+    
+    return rows
+
+def format_responses(response:list[tuple]) -> dict:
+    #Check if every company has an empty list. If so, return no data message
+    is_empty = True
+    for comp in response: 
+        if comp[1]: 
+            is_empty = False
+            break
+        
+    if not response or is_empty: raise ValueError
+    final_data = []
+    for (company_name,rows) in response:
+        company_data = dict(
+            company=company_name,
+            data=[]
+        )
+        for row in rows:
+            temp_date_data = dict(
+                Date=row[0],
+                Open=row[1],
+                High=row[2],
+                Low=row[3],
+                Close=row[4],
+                Adj_Close=row[5],
+                Volume=row[6],
+            )
+            company_data['data'].append(temp_date_data)
+        final_data.append(company_data)
+    return final_data 
